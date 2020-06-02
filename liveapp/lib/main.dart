dd21f7d8e5c56f73f4e1eb52ce1c9ffd14f02d24
Sagar <sagarsandy492@gmail.com>
2020-06-02 11:28:31 +0530

    video-player-added

--- liveapp/lib/main.dart
+++ liveapp/lib/main.dart
@@ -56,6 +56,49 @@ class _MyHomePageState extends State<MyHomePage>
     print(diskStorage.getInt('userId'));
   }
 
+  List<Course> _trendingCourses = [
+    Course(
+      id: 1,
+      image: "https://i.picsum.photos/id/1/150/150.jpg",
+      price: 11,
+      title: "Introduction to docker and docking",
+      author: "Docker Mastery",
+      category: "Category 1",
+    ),
+    Course(
+      id: 2,
+      image: "https://i.picsum.photos/id/2/150/150.jpg",
+      price: 21,
+      title: "Jump to javascript in 60 days as a master in programming",
+      author: "Javascript Learners Library",
+      category: "Category 2",
+    ),
+    Course(
+      id: 3,
+      image: "https://i.picsum.photos/id/3/150/150.jpg",
+      price: 31,
+      title: "The ultimate guide to programers and techies",
+      author: "Programming Library",
+      category: "Category 3",
+    ),
+    Course(
+      id: 4,
+      image: "https://i.picsum.photos/id/4/150/150.jpg",
+      price: 41,
+      title: "Web development bootcamp by Sagar Sandy this is long",
+      author: "Web Developers boot camps",
+      category: "Category 4",
+    ),
+    Course(
+      id: 5,
+      image: "https://i.picsum.photos/id/5/150/150.jpg",
+      price: 51,
+      title: "Feedback sessions by feedback masters",
+      author: "Feedback foliers",
+      category: "Category 5",
+    ),
+  ];
+
   Future<List<Course>> getTrendingCourses() async {
     const url = "https://sagarsandy492.mock.pw/api/courses";
     final response = await get(url);
@@ -191,7 +234,6 @@ class _MyHomePageState extends State<MyHomePage>
   List<Course> _categoryCourses1 = [];
   List<Course> _categoryCourses2 = [];
   List<Course> _categoryCourses3 = [];
-  List<Course> _trendingCourses = [];
 
   @override
   Widget build(BuildContext context) {
@@ -281,7 +323,7 @@ class _MyHomePageState extends State<MyHomePage>
             padding: EdgeInsets.only(left: 15),
             child: Column(
               children: <Widget>[
-                HomePageTitlePadding("Top Courses in Development", 15.0),
+                HomePageTitlePadding("Trending Courses", 15.0),
                 CoursesListBlockOneWidget(_trendingCourses),
                 HomePageTitlePadding("Top Courses in Development", 1.0),
                 CoursesListBlockTwoWidget(_categoryCourses1),
--- liveapp/lib/screens/CourseDetailsScreen.dart
+++ liveapp/lib/screens/CourseDetailsScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:liveapp/models/Content.dart';
 import 'package:liveapp/models/Course.dart';
 import 'package:liveapp/models/Section.dart';
+import 'package:video_player/video_player.dart';
 
 class CourseDetailsScreen extends StatefulWidget {
   final Course course;
@@ -156,6 +157,9 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
   ];
 
   ScrollController _scrollController;
+  VideoPlayerController _videoPlayerController;
+  Future<void> _initializeVideoPlayerFuture;
+
   bool hideTitle = false;
   String descText =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
@@ -164,6 +168,13 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
   @override
   void initState() {
     super.initState();
+
+    _videoPlayerController = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    _initializeVideoPlayerFuture = _videoPlayerController.initialize();
+
     _scrollController = ScrollController();
 
     _scrollController.addListener(() {
@@ -183,6 +194,15 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
   }
 
   @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _videoPlayerController.dispose();
+    _scrollController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: Container(
@@ -195,6 +215,27 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
             style: TextStyle(
                 fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
           ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown
+          setState(() {
+            // If the video is playing, pause it.
+            if (_videoPlayerController.value.isPlaying) {
+              _videoPlayerController.pause();
+            } else {
+              // If the video is paused, play it.
+              _videoPlayerController.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _videoPlayerController.value.isPlaying
+              ? Icons.pause
+              : Icons.play_arrow,
         ),
       ),
       body: CustomScrollView(
@@ -243,10 +284,91 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
                               elevation: 10,
                             ),
                           ),
-                          Chip(label: Text("Development")),
+                          GestureDetector(
+                            child: Chip(
+                              label: Text("Development"),
+                            ),
+                            onTap: () {
+                              _videoPlayerController.play();
+                              print("Hello world start");
+                              print(_videoPlayerController.value.aspectRatio);
+                              print("Hello world end");
+                            },
+                          ),
                         ],
                       ),
                 preferredSize: null),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 10, left: 10, bottom: 0, right: 10),
+                    child: Text(
+                      "Preview",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Stack(
+                    children: <Widget>[
+                      FutureBuilder(
+                        future: _initializeVideoPlayerFuture,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            // If the VideoPlayerController has finished initialization, use
+                            // the data it provides to limit the aspect ratio of the video.
+                            return Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: AspectRatio(
+                                aspectRatio:
+                                    _videoPlayerController.value.aspectRatio,
+                                // Use the VideoPlayer widget to display the video.
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: VideoPlayer(_videoPlayerController),
+                                ),
+                              ),
+                            );
+                          } else {
+                            // If the VideoPlayerController is still initializing, show a
+                            // loading spinner.
+                            return Center(child: CircularProgressIndicator());
+                          }
+                        },
+                      ),
+                      Center(
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            // Wrap the play or pause in a call to `setState`. This ensures the
+                            // correct icon is shown
+                            setState(() {
+                              // If the video is playing, pause it.
+                              if (_videoPlayerController.value.isPlaying) {
+                                _videoPlayerController.pause();
+                              } else {
+                                // If the video is paused, play it.
+                                _videoPlayerController.play();
+                              }
+                            });
+                          },
+                          // Display the correct icon depending on the state of the player.
+                          child: Icon(
+                            _videoPlayerController.value.isPlaying
+                                ? Icons.pause
+                                : Icons.play_arrow,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
           SliverToBoxAdapter(
             child: Container(
@@ -297,13 +419,6 @@ class _CourseDetailsScreenState extends State<CourseDetailsScreen> {
               ),
             ),
           ),
-//          SliverToBoxAdapter(
-//            child: Container(
-//              width: double.infinity,
-//              height: 300,
-//              color: Colors.red,
-//            ),
-//          ),
           SliverList(
             delegate: SliverChildBuilderDelegate((context, index) {
               if (index == 0) {
